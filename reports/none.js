126eb354ffd732be8626565a87156d681d495da2
Tom Leen <tml@leentop.thomasleen.com>
2017-08-09 10:35:10 -0400

    Add empty report: none

---
+++ reports/none.js
@@ -0,0 +1,5 @@
+// This report will display information about one or more exercises
+// details on a per track basis
+module.exports = function(configfile, targets){
+  return 'No report specified';
+};
